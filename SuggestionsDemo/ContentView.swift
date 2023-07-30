60e484d4ab45c926e469bce0a3b9cbe168f9e221
Dan Davison <dandavison7@gmail.com>
2023-07-30 15:56:52 -0400

    Get rid of other window content

--- SuggestionsDemo/ContentView.swift
+++ SuggestionsDemo/ContentView.swift
@@ -11,23 +11,9 @@ struct ContentView: View {
     @StateObject var model = DictionaryModel()
     
     var body: some View {
-        VStack(spacing: 40) {
-            VStack(spacing: 40) {
-                if let currentTranslation = self.model.currentTranslation {
-                    Text(self.model.currentText)
-                    Image(systemName: "arrow.right")
-                    Text(currentTranslation)
-                }
-            }
-            .font(.title)
-            .frame(width: 600, height: 300)
-            .toolbar {
-                SuggestionInput(text: self.$model.currentText,
-                                suggestionGroups: self.model.suggestionGroups)
-                    .frame(width: 300)
-            }
-        }
-        .padding()
+        SuggestionInput(text: self.$model.currentText,
+                        suggestionGroups: self.model.suggestionGroups)
+        .frame(width: 300)
     }
 }
 
